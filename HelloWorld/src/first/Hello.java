a678b79c007027833e8ddee1db6f76a8b4dda14c
dir-FY25murayamafumiya <FumiyaMurayama@Dirbato-1946x>
2025-05-23 11:44:28 +0900

    Gitの練習です

--- HelloWorld/src/first/Hello.java
+++ HelloWorld/src/first/Hello.java
@@ -3,8 +3,8 @@ package first;
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello,World!");
-     
+      System.out.println("Hello,World!");
+      System.out.print("こんにちわ、世界！”);
 
     }
 
